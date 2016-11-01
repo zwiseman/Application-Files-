e86f4476d01d9ffabec85cf477ea3c8a9f3eb54a
Andrew Madden <acmaddencs@gmail.com>
2016-11-01 16:12:45 -0400

    Comment on fix

--- ThirdViewController.swift
+++ ThirdViewController.swift
@@ -31,13 +31,19 @@ class ThirdViewController: UIViewController, UITableViewDataSource, UITableViewD
     func itemsDownloaded(items: NSArray) {
         
         feedItems = items
+	//From what I can see you are using parseModel.swift to pull in your data for feedItems
+	//So I'm assuming that you're data is being pulled in correctly and feedItems.count > 0
+	//then you need to reload the tableData, which you almost got right.
+	//So this line -> self.myTable.reloadData() will crash the application because you're trying
+	//to modify the UI from a background thread which is a no-no.
+	//Corrected code would be (because you have the dataSource set to self) -> DispatchQueue.main.async{self.myTable.reloadData()}
+
         self.myTable.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
          //Return the number of feed items
-        return feedItems.count
-        
+        return feedItems.count        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
